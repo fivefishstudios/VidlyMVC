3041f6c1ddc0a42d3f38a9ec45f2427d382925fb
RuelOquindo <ruel@fivefish.net>
2019-04-29 13:50:39 -0500

    populate movie DB

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -9,31 +9,42 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        private List<Movie> _movies;
+
+
+        private MyDBContext _context;
+
         public MoviesController()
         {
-            _movies = new List<Movie>()
-            {
-                new Movie() { Name = "Mission Impossible", Id=1, Actor = "Tom Cruise" },
-                new Movie() { Name = "Top Gun", Id=2, Actor = "Tom Cruise"},
-                new Movie() { Name = "Ghost Protocol", Id=3, Actor = "John Doe"},
-                new Movie() { Name = "Shrek", Id=4, Actor = "Shrek as himself"},
-                new Movie() { Name = "James Bond", Id=5, Actor = "Roger Moore"}
-            };
+            _context = new MyDBContext();
         }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
 
         // GET: Movies/Random
         public ActionResult Index()
         {
-            return View(_movies);
+            var movies = GetAllMovies();
+            return View(movies);
         }
 
         // GET: Movies/DetailView/99
         [Route("Movies/DetailView/{id}")]
         public ActionResult View(int id)
         {
-            Movie movie = _movies.Find(x => x.Id == id);
+           //  Movie movie = GetAllMovies().SingleOrDefault(x => x.Id == id);
+            Movie movie = _context.Movies.SingleOrDefault(x => x.Id == id);
             return View(movie);
+        }
+
+
+        private IEnumerable<Movie> GetAllMovies()
+        {
+            var _movies = _context.Movies;
+            return _movies;
         }
 
     }
