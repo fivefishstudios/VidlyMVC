efcea02b2ef2ac1d54cc203421def454777da428
RuelOquindo <ruel@fivefish.net>
2019-04-28 20:31:13 -0500

    initial commit; working movies and customers list/view

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        List<Customer> _customers;
+        public CustomersController()
+        {
+            _customers = new List<Customer>()
+            {
+                new Customer() { Id=1, Name="Roberto Gonza", City="Brentwood" },
+                new Customer() { Id=2, Name="John Doe", City="Nashville" },
+                new Customer() { Id=49, Name="Mary Poppins", City="Franklin"}
+            };
+            
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+                
+            return View(_customers);
+        }
+
+        public ActionResult View(int Id)
+        {
+            Customer customer = _customers.Find(x => x.Id == Id);
+            return View(customer);
+        }
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private List<Movie> _movies;
+        public MoviesController()
+        {
+            _movies = new List<Movie>()
+            {
+                new Movie() { Name = "Mission Impossible", Id=1, Actor = "Tom Cruise" },
+                new Movie() { Name = "Top Gun", Id=2, Actor = "Tom Cruise"},
+                new Movie() { Name = "Ghost Protocol", Id=3, Actor = "John Doe"},
+                new Movie() { Name = "Shrek", Id=4, Actor = "Shrek as himself"},
+                new Movie() { Name = "James Bond", Id=5, Actor = "Roger Moore"}
+            };
+        }
+
+        // GET: Movies/Random
+        public ActionResult Index()
+        {
+            return View(_movies);
+        }
+
+        // Movies/View/99
+        [Route("Movies/View/{id}")]
+        public ActionResult View(int id)
+        {
+            Movie movie = _movies.Find(x => x.Id == id);
+            return View(movie);
+        }
+
+    }
+}
