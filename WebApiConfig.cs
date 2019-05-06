b27b06374c02eea7b9687129ecc5e6adfda99fc7
RuelOquindo <ruel@fivefish.net>
2019-05-06 00:38:35 -0500

    Customer api now working

---
+++ WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace Vidly
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //throw new System.NotImplementedException();
+
+            config.MapHttpAttributeRoutes();
+
+            _ = config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+        }
+    }
+}
