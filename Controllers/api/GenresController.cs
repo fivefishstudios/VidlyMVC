84983e4477e019ef84693286204b4d6f09c0e735
RuelOquindo <ruel@fivefish.net>
2019-05-07 22:35:40 -0500

    api for customer, movies and genre

---
+++ Controllers/api/GenresController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class GenresController : ApiController
+    {
+        private readonly MyDBContext _context;
+
+        public GenresController()
+        {
+            _context = new MyDBContext();
+        }
+
+        // GET /api/genres
+        // get all genre records
+        public IEnumerable<Genre> GetGenres()
+        {
+            var genres = _context.Genre.ToList();
+            return genres;
+        }
+
+        // GET /api/genres/1
+        // get genre with specific id#
+        public Genre GetGenre(int id)
+        {
+            var genre = _context.Genre.SingleOrDefault(g => g.Id == id);
+            if (genre != null)
+            {
+                // record found
+                return genre;
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        // DELETE /api/genres/1
+        // delete genre with specific id#
+        [HttpDelete]
+        public void DeleteGenre(int id)
+        {
+            var genre = _context.Genre.SingleOrDefault(g => g.Id == id);
+            if (genre != null)
+            {
+                // record id found, now delete this
+                _ = _context.Genre.Remove(genre);  // this just removes from memory
+                _ = _context.SaveChanges();
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        // POST /api/genre/
+        // add a new genre record
+        [HttpPost]
+        public Genre CreateGenre(Genre genre)
+        {
+            // validate model
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            // if good, save
+            _ = _context.Genre.Add(genre);
+            _ = _context.SaveChanges();
+
+            // return model
+            return genre;
+        }
+
+        // PUT /api/genres/1
+        // update specific genre record
+        [HttpPut]
+        public Genre UpdateGenre(int id, Genre genre)
+        {
+            // validate model
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            // check if record exists
+            var genreInDb = _context.Genre.SingleOrDefault(g => g.Id == id);
+            if (genreInDb != null)
+            {
+                // record id found, now update this
+                genreInDb.GenreName = genre.GenreName;
+                _ = _context.SaveChanges();
+                return genreInDb;
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
---
+++ Controllers/api/MoviesController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using Microsoft.Ajax.Utilities;
+using Vidly.Models;
+
+namespace Vidly.Controllers.api
+{
+    public class MoviesController : ApiController
+    {
+        // setup context so we can access the database
+        private MyDBContext _context;
+
+        public MoviesController()
+        {
+            _context = new MyDBContext();
+        }
+
+        // GET /api/movies
+        // get all movies
+        public IEnumerable<Movie> GetMovies()
+        {
+            var movies = _context.Movies.ToList();
+            return movies;
+        }
+
+        // GET /api/movies/1
+        // get movie with specific id
+        public Movie GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                // found the movie
+                return movie;
+            }
+        }
+
+        // DELETE /api/movies/1
+        // delete movie with specific id
+        [HttpDelete]
+        public void DeleteMovie(int id)
+        {
+            // look for this movie id
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movieInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                // record found, now delete it
+                _context.Movies.Remove(movieInDb);
+                _context.SaveChanges();
+            }
+        }
+
+        // POST /api/movie
+        // add a new movie to database
+        [HttpPost]
+        public Movie AddMovie(Movie movie)
+        {
+            // validate movie
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            // add movie to memory
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            // return what we just added
+            return movie;
+        }
+
+        // PUT /api/movie/1
+        // update movie record with new data
+        [HttpPut]
+        public Movie UpdateMovie(int id, Movie movie)
+        {
+            // validate movie
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            // find movie record
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            // if not found, return error
+            if (movieInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                // if found, update individual fields
+                movieInDb.Id = id;
+                movieInDb.Name = movie.Name;
+                movieInDb.Actor = movie.Actor;
+                movieInDb.DateAdded = movie.DateAdded;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.Rating = movie.Rating;
+                movieInDb.Stock = movie.Stock;
+                movieInDb.GenreId = movie.GenreId;
+            }
+
+            // save to database
+            _context.SaveChanges();
+
+            // return record
+            return movieInDb;
+        }
+    }
+}
